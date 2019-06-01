6bd12c4e1e3c61894ba0ab0ded0c20e444ded22c
koukaakiva <koukaakiva@gmail.com>
2019-06-01 03:23:55 -0400

    Got TextBoxes working and added left and center ones.

--- Scripts/TextBox.gd
+++ Scripts/TextBox.gd
@@ -1,20 +1,19 @@
 extends Control
 
+#TODO: Set these based on the font.
 const CHAR_HEIGHT = 25;
 const CHAR_WIDTH = 24;
-const LABLE_WIDTH = 876;
-onready var label = $Panel/NinePatchRect/MarginContainer/HBoxContainer/RichTextLabel;
-#onready var LABLE_WIDTH = label.get_size().x;
+const CONTENT_MIN_HEIGHT = 128;
+const BORDER_WIDTH = 8;
+onready var label = $Panel/NinePatchRect/RichTextLabel;
+#TODO: Divide this by the size of a char and count up by 1 below.
+onready var LABLE_WIDTH = label.get_size().x;
 onready var ready = true;
-
-func _ready():
-	pass;
 
 func setText(text: String) -> void:
 	if(!ready):
 		yield(self, "ready");
 	label.set_text(text);
-	#var height = (label.get_line_count() * CHAR_HEIGHT) - 10;
 	var height = 1;
 	var remainingWidth = LABLE_WIDTH;
 	var wordLength = 0;
@@ -34,9 +33,12 @@ func setText(text: String) -> void:
 				height += 1;
 				remainingWidth = LABLE_WIDTH;
 				wordLength = 0;
+	if(wordLength > remainingWidth):
+		height += 1;
+	if((text.length() != 0) && (text[text.length() - 1] == '\n')):
+		height -= 1;
 	height *= CHAR_HEIGHT;
-	if(height < 144):
-		height = 144;
-	#label.rect_min_size = Vector2(LABLE_WIDTH, 128);
-	self.rect_min_size = Vector2(0, height);
-	print(label.get_size().x);
+	if(height < CONTENT_MIN_HEIGHT):
+		height = CONTENT_MIN_HEIGHT;
+	self.rect_min_size = Vector2(0, height + (BORDER_WIDTH * 2));
+	label.rect_min_size = Vector2(LABLE_WIDTH, height);
