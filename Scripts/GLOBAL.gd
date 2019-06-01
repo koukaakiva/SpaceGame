896b092b040cbe05a7342f8b5df606ce2c7edf85
koukaakiva <koukaakiva@gmail.com>
2019-06-01 05:47:47 -0400

    Refixed TextBox. Now only uses one scene and an enum to choose icon location.

---
+++ Scripts/GLOBAL.gd
@@ -0,0 +1,4 @@
+extends Node
+
+onready var textbox = load("res://Scenes/Textbox.tscn");
+enum textbox_icon_type {NONE, LEFT, RIGHT};
--- Scripts/TextBox.gd
+++ Scripts/TextBox.gd
@@ -5,14 +5,27 @@ const CHAR_HEIGHT = 25;
 const CHAR_WIDTH = 24;
 const CONTENT_MIN_HEIGHT = 128;
 const BORDER_WIDTH = 8;
-onready var label = $Panel/NinePatchRect/RichTextLabel;
+const ICONLESS_LABLE_WIDTH = 968;
+const ICONED_LABLE_WIDTH = ICONLESS_LABLE_WIDTH - 132;
+onready var contentContainer = $Panel/NinePatchRect/MarginContainer/ContentContainer;
+onready var label = $Panel/NinePatchRect/MarginContainer/ContentContainer/Label;
+onready var icon = load("res://Scenes/Icon.tscn");
 #TODO: Divide this by the size of a char and count up by 1 below.
-onready var LABLE_WIDTH = label.get_size().x;
 onready var ready = true;
+var LABLE_WIDTH;
 
-func setText(text: String) -> void:
+func initiate(icon_type, text: String) -> void:
 	if(!ready):
 		yield(self, "ready");
+	if(icon_type == GLOBAL.textbox_icon_type.LEFT):
+		contentContainer.add_child(icon.instance());
+		contentContainer.move_child(label, 1);
+		LABLE_WIDTH = ICONED_LABLE_WIDTH;
+	elif(icon_type == GLOBAL.textbox_icon_type.RIGHT):
+		contentContainer.add_child(icon.instance());
+		LABLE_WIDTH = ICONED_LABLE_WIDTH;
+	else:
+		LABLE_WIDTH = ICONLESS_LABLE_WIDTH;
 	label.set_text(text);
 	var height = 1;
 	var remainingWidth = LABLE_WIDTH;
@@ -41,4 +54,5 @@ func setText(text: String) -> void:
 	if(height < CONTENT_MIN_HEIGHT):
 		height = CONTENT_MIN_HEIGHT;
 	self.rect_min_size = Vector2(0, height + (BORDER_WIDTH * 2));
-	label.rect_min_size = Vector2(LABLE_WIDTH, height);
+	if(icon_type == GLOBAL.textbox_icon_type.NONE):
+		label.rect_min_size = Vector2(LABLE_WIDTH, height);
